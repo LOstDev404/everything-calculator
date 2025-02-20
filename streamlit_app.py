2519426d22634e75b93553d30c154e83c60145fe
LOstDev404 <127157866+LOstDev404@users.noreply.github.com>
2025-02-20 05:15:21 +0000

    Initial Commit 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,10 @@ import streamlit as st
 st.set_page_config(
     page_title="E-Calc",
 )
-st.title("Everything Calculator)
+st.title("Everything Calculator")
+st.error("None of this is done... yet")
+used_calculator = st.selectbox('Choose a calculator:', ['What is this?'])
+if used_calculator == 'What is this?':
+    st.write('The everything calculator (E-Calc) is a colection of different calculators that can be used to calculate different things.')
+
+    
